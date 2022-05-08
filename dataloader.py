f53f116755cd8fab1b6e78bcd102bfaf21a2c01a
Rene Emmaneel <reneemmaneel00@gmail.com>
2022-05-08 21:04:24 +0200

    Single task adapters loading/training and added datasets

--- dataloader.py
+++ dataloader.py
@@ -3,31 +3,80 @@
 from datasets import load_dataset
 from transformers import BertTokenizer
 
-def load_and_process_dataset(dataset, encode_batch, label_name):
-    # Encode the input data
-    dataset = dataset.map(encode_batch, batched=True)
+def load_and_process_dataset(dataset, encode_batch, label_name, labels=None):
     # The transformers model expects the target class column to be named "labels"
     dataset = dataset.rename_column(label_name, "labels")
+    # Encode the input data
+    dataset = dataset.map(encode_batch, batched=True)
+
     # Transform to pytorch tensors and only output the required columns
     dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 
-    id2label = {id: label for (id, label) in enumerate(dataset["train"].features["labels"].names)}
+    if labels is None:
+        id2label = {id: label for (id, label) in enumerate(dataset["train"].features["labels"].names)}
+    else:
+        id2label = {id: label for (id, label) in enumerate(labels)}
+
     return dataset, id2label
 
-def load_specific_dataset(dataset_name, task_name, inputs, label_name):
+def load_specific_dataset(dataset_name, task_name, inputs, label_name, labels=None):
     dataset = load_dataset(dataset_name, task_name)
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+    if not labels is None:
+        label2id = {label: id for (id, label) in enumerate(labels)}
+
     def encode_batch(batch):
+        """Encodes a batch of input data using the model tokenizer.
+        If the labels are not given as ints, change them to ints"""
         batch_inputs = [batch[name] for name in inputs]
-        """Encodes a batch of input data using the model tokenizer."""
-        return tokenizer(
+        tokens = tokenizer(
             *batch_inputs,
             max_length=180,
             truncation=True,
-            padding="max_length"
+            padding="max_length",
+            add_special_tokens=True
         )
-    return load_and_process_dataset(dataset, encode_batch, label_name)
+        if not labels is None:
+            tokens["labels"] = [label2id[label] if label in label2id else -1 for label in batch["labels"]]
+        return tokens
+    return load_and_process_dataset(dataset, encode_batch, label_name, labels=labels)
+
+def load_specific_multi_choice_dataset(dataset_name, task_name, sentence_a_inputs, sentence_b_inputs, label_name, labels=None):
+    """Load datasets with multiple options as inputs, e.g. quess the correct ending"""
+    dataset = load_dataset(dataset_name, task_name)
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+    if not labels is None:
+        label2id = {label: id for (id, label) in enumerate(labels)}
+    def encode_batch(batch):
+        """Encodes a batch of input data using the model tokenizer.
+        If the labels are not given as ints, change them to ints"""
+        len_batch = len(batch[sentence_a_inputs[0]])
+        num_choice = len(sentence_b_inputs)
+
+        all_encoded = {"input_ids": [], "attention_mask": []}
+        batch_inputs_a = [batch[name] for name in sentence_a_inputs]
+        batch_inputs_b = [batch[name] for name in sentence_b_inputs]
+        for i in range(len_batch):
+            sentences_a = [' '.join([column[i] for column in batch_inputs_a]) for _ in range(num_choice)]
+            sentences_b = [column[i] for column in batch_inputs_b]
+            encoded = tokenizer(
+                sentences_a,
+                sentences_b,
+                max_length=180,
+                truncation=True,
+                padding="max_length",
+            )
+            all_encoded["input_ids"].append(encoded["input_ids"])
+            all_encoded["attention_mask"].append(encoded["attention_mask"])
+        if not labels is None:
+            all_encoded["labels"] = [label2id[label] if label in label2id else -1 for label in batch["labels"]]
+        else:
+            all_encoded["labels"] = batch["labels"]
+        return all_encoded
+    return load_and_process_dataset(dataset, encode_batch, label_name, labels=labels)
+
 
 def load_dataset_by_name(name):
     if name == "mnli":
@@ -37,27 +86,31 @@ def load_dataset_by_name(name):
     elif name == "sst":
         return load_specific_dataset("glue", "sst2", ["sentence"], "label")
     elif name == "wgrande":
-        return load_specific_dataset("winogrande", "winogrande_xl", ["sentence", "option1", "option2"], "answer")
+        return load_specific_multi_choice_dataset("winogrande", "winogrande_xl", ["sentence"], ["option1", "option2"], "answer", labels=["1", "2"])
     elif name == "imdb":
         return load_specific_dataset("imdb", "plain_text", ["text"], "label")
     elif name == "hswag":
-        return load_specific_dataset("hellaswag", "default", ["activity_label", "ctx", "ctx_a", "ctx_b", "endings"], "label")
+        raise NotImplementedError()
+        #return load_specific_dataset("hellaswag", "default", ["activity_label", "ctx", "endings"], "label")
     elif name == "siqa":
-        return load_specific_dataset("social_i_qa", "default", ["context", "question", "answerA", "answerB", "answerC"], "label")
+        #TODO: test
+        return load_specific_multi_choice_dataset("social_i_qa", "default", ["context", "question"], ["answerA", "answerB", "answerC"], "label")
     elif name == "cqa":
-        return load_specific_dataset("cosmos_qa", "default", ["id", "context", "question", "answer0", "answer1", "answer2", "answer3"], "label")
+        #TODO: test
+        return load_specific_multi_choice_dataset("cosmos_qa", "default", ["context", "question"], ["answer0", "answer1", "answer2", "answer3"], "label")
     elif name == "scitail":
-        return load_specific_dataset("scitail", "snli_format", ["premise", "hypothesis"], "label")
+        return load_specific_dataset("scitail", "tsv_format", ["premise", "hypothesis"], "label", labels=["neutral", "entails"])
     elif name == "argument":
         raise NotImplementedError() #I can't find this dataset
     elif name == "csqa":
-        raise NotImplementedError() #This dataset requires a bit more work, will do it later
+        #TODO: test
+        return load_specific_multi_choice_dataset("cosmos_qa", "default", ["context", "question"], ["answer0", "answer1", "answer2", "answer3"], "label")
     elif name == "boolq":
-        return load_specific_dataset("boolq", "default", ["question", "passage"], "answer")
+        return load_specific_dataset("boolq", "default", ["question", "passage"], "answer", labels=["true", "false"])
     elif name == "mrpc":
         return load_specific_dataset("glue", "mrpc", ["sentence1", "sentence2"], "label")
     elif name == "sick":
-        raise NotImplementedError() #This dataset requires a bit more work as well, will do it later
+        return load_specific_dataset("sick", "default", ["sentence_A", "sentence_B", "en"], "label")
     elif name == "rte":
         return load_specific_dataset("glue", "rte", ["sentence1", "sentence2"], "label")
     elif name == "cb":
---
+++ single_task_adapters.py
@@ -0,0 +1,130 @@
+"""Replicating table 1 from the AdapterFusion paper"""
+
+from datasets import load_dataset
+
+import transformers
+from transformers import BertTokenizer, EarlyStoppingCallback
+from transformers import BertConfig, BertModelWithHeads
+from transformers import TrainingArguments, AdapterTrainer, EvalPrediction
+from transformers.adapters.composition import Fuse
+transformers.logging.set_verbosity_error()
+
+import numpy as np
+import torch
+import argparse
+
+import dataloader as dataloader
+
+def load_bert_model(id2label):
+    config = BertConfig.from_pretrained(
+        "bert-base-uncased",
+        id2label=id2label,
+    )
+    model = BertModelWithHeads.from_pretrained(
+        "bert-base-uncased",
+        config=config,
+    )
+    return model
+
+def setup_adapter(model, id2label, task):
+    if task == "mnli":
+        model.load_adapter("nli/multinli@ukp", load_as="multinli")
+    elif task == "qqp":
+        model.load_adapter("sts/qqp@ukp", load_as="qqp")
+    elif task == "sst":
+        model.load_adapter("sentiment/sst-2@ukp", load_as="sst")
+    elif task == "wgrande":
+        model.load_adapter("comsense/winogrande@ukp", load_as="wgrande")
+        model.add_multiple_choice_head("wgrande", num_choices=2)
+    elif task == "imdb":
+        model.load_adapter("sentiment/imdb@ukp", load_as="imdb")
+        model.add_classification_head("imdb", num_labels=len(id2label))
+    elif task == "hswag":
+        model.load_adapter("comsense/hellaswag@ukp", load_as="hswag")
+    elif task == "siqa":
+        model.load_adapter("comsense/siqa@ukp", load_as="siqa")
+    elif task == "cqa":
+        model.load_adapter("comsense/cosmosqa@ukp", load_as="cqa")
+    elif task == "scitail":
+        model.load_adapter("nli/scitail@ukp", load_as="scitail")
+        model.add_classification_head("scitail", num_labels=len(id2label))
+    elif task == "argument":
+        model.load_adapter("argument/ukpsent@ukp", load_as="argument")
+    elif task == "csqa":
+        model.load_adapter("comsense/csqa@ukp", load_as="csqa")
+    elif task == "boolq":
+        model.load_adapter("qa/boolq@ukp", load_as="boolq")
+    elif task == "mrpc":
+        model.load_adapter("sts/mrpc@ukp", load_as="mrpc")
+    elif task == "sick":
+        model.load_adapter("nli/sick@ukp", load_as="sick")
+    elif task == "rte":
+        model.load_adapter("nli/rte@ukp", load_as="rte")
+    elif task == "cb":
+        model.load_adapter("nli/cb@ukp", load_as="cb")
+        model.add_classification_head("cb", num_labels=len(id2label))
+    else:
+        raise NotImplementedError()
+
+    # Set active adapter
+    model.set_active_adapters(task)
+    model.train_adapter(task)
+    return model
+
+def compute_accuracy(p: EvalPrediction):
+    preds = np.argmax(p.predictions, axis=1)
+    return {"acc": (preds == p.label_ids).mean()}
+
+def evaluate_model(model, dataset):
+    trainer = AdapterTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["validation"],
+        compute_metrics=compute_accuracy,
+    )
+    with open('results.txt', 'a') as outfile:
+        outfile.write('eval acc pre-training: ' + str(trainer.evaluate()["eval_acc"]) + '\n')
+    trainer.train()
+    with open('results.txt', 'a') as outfile:
+        outfile.write('eval acc post-training: ' + str(trainer.evaluate()["eval_acc"]) + '\n')
+
+training_args = TrainingArguments(
+    learning_rate=1e-4,
+    num_train_epochs=1,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    logging_steps=100,
+    output_dir="training_output",
+    overwrite_output_dir=True,
+    # The next line is important to ensure the dataset labels are properly passed to the model
+    remove_unused_columns=False,
+)
+
+if __name__ == '__main__':
+    #tasks = ["mrpc", "scitail", "qqp", "wgrande", "hswag", "imdb", "siqa", "cqa", "argument", "csgq", "boolq", "rte", "cb"]
+
+    #tasks = ["mrpc", "scitail", "qqp", "sst", "wgrande", "hswag", "imdb", "siqa", "cqa", "argument", "csgq", "boolq", "rte", "cb"]
+
+    tasks = ["wgrande"]
+
+    for task in tasks:
+        with open('results.txt', 'a') as outfile:
+            outfile.write(f"[Evaluating the adapters for task {task}]\n")
+        #try:
+        dataset, id2label = dataloader.load_dataset_by_name(task)
+        except Exception as e:
+            with open('results.txt', 'a') as outfile:
+                outfile.write(f"Couldn't load dataset because of exception: \n")
+                outfile.write(str(e) + '\n')
+            continue
+
+        try:
+            model = load_bert_model(id2label)
+            model = setup_adapter(model, id2label, task)
+        except Exception as e:
+            with open('results.txt', 'a') as outfile:
+                outfile.write(f"Couldn't load adapter because of exception: \n")
+                outfile.write(str(e) + '\n')
+            continue
+        evaluate_model(model, dataset)
