3653370d07afbfb21cbb3fa08d9b358c94350c25
bartvanvulpen <bartvanvulpen@me.com>
2022-05-28 22:23:41 +0200

    meta-test mem fix and deepcopy fix

--- meta_test.py
+++ meta_test.py
@@ -36,86 +36,46 @@ def test_protomaml(model, task, k_shot=4, max_it=20, full_dl_batch_size=8):
 
     pl.seed_everything(42)
     model = model.to(device)
-
     model_clean = deepcopy(model)
 
     num_classes = get_num_classes(task)
-    
-    if k_shot == 8 and num_classes > 2:
-        print('Applying 2x4 with', 'K_SHOT:', k_shot // 2)
+    n_adapters = len(model.hparams.adapters_used)
 
-        # get test dataloaders and sampler
-        full_test_loader, sample_test_loader, sampler = get_test_loaders(task, K_SHOT=k_shot // 2,
-                                                                         full_dl_batch_size=full_dl_batch_size,
-                                                                         num_workers=0)
-        # Select the k-shot batch and finetune
-        accuracies = []
-        indices = []
-
-        i = 0
-        ev_i = 0
-        for x, support_indices in tqdm(zip(sample_test_loader, sampler), "Performing few-shot finetuning"):
-            i += 1
-            support_inputs = {'input_ids': x[1][0].to(device), 'token_type_ids': x[1][1].to(device),
-                              'attention_mask': x[1][2].to(device)}
-            support_targets = x[2].to(device)
-
-            print('--- Iteration:', i)
-
-            # Finetune new model on support set
-            local_model, output_weight, output_bias, classes = model.adapt_few_shot(
-                support_inputs, support_targets
-            )
-            
-            model.model = local_model
-            indices.append(support_indices)
-
-            if i == 2:
-                print('evaluating on local model...')
-                i = 0
-                support_indices = [item for sublist in indices for item in sublist]
-                indices = []
-                model = model_clean
-                print('support indices:', support_indices)
-
-                ev_i += 1
-
-                # get accuracy of finetuned model on full dataset
-                with torch.no_grad():
-                    local_model.eval()
-                    batch_acc = torch.zeros((0,), dtype=torch.float32, device=device)
-
-                    for q_data in full_test_loader:
-                        query_inputs = {'input_ids': q_data[1][0].to(device), 'token_type_ids': q_data[1][1].to(device),
-                                        'attention_mask': q_data[1][2].to(device)}
-
-                        query_targets = q_data[2].to(device)
-                        query_labels = (
-                            (classes[None, :] == query_targets[:, None]).long().argmax(dim=-1)
-                        )
-                        _, _, acc = model.run_model(
-                            local_model, output_weight, output_bias, query_inputs, query_labels
-                        )
-                        
-                        batch_acc = torch.cat([batch_acc, acc.detach()], dim=0)
-
-                    # exclude support set elements
-                    for s_idx in support_indices:
-                        batch_acc[s_idx] = 0
-                    batch_acc = batch_acc.sum().item() / (
-                            batch_acc.shape[0] - len(support_indices)
-                    )
-                    accuracies.append(batch_acc)
-
-                # return mean accuracy over the runs after max iterations
-                if ev_i == max_it:
-                    return mean(accuracies), stdev(accuracies)
         
-    elif k_shot == 16:
-        print('Applying 4x4 with', 'K_SHOT:', k_shot//4)
+    if k_shot == 16 or \
+        (k_shot == 8 and num_classes > 2 and n_adapters < 10) or \
+        (k_shot == 4 and num_classes > 2 and n_adapters == 16) or \
+        (k_shot == 8 and num_classes > 2 and n_adapters == 16) or \
+        (k_shot == 8 and num_classes < 3 and n_adapters == 16):
 
-        # get test dataloaders and sampler
-        full_test_loader, sample_test_loader, sampler = get_test_loaders(task, K_SHOT=k_shot//4,
+        if k_shot == 16:
+            k_it = 4
+            new_k = 4
+            print('Applying 4x4 to get total k=16')
+
+        elif (k_shot == 8 and num_classes > 2 and n_adapters < 10):
+            k_it = 2
+            new_k = 4
+            print('Applying 2x4 to get total k=8, because num_classes > 2')
+
+        elif (k_shot == 4 and num_classes > 2 and n_adapters == 16):
+            k_it = 2
+            new_k = 2
+            print('Applying 2x2 to get total k=4, because all adapters are used and num_classes > 2')
+
+        elif (k_shot == 8 and num_classes > 2 and n_adapters == 16):
+            k_it = 4
+            new_k = 2
+            print('Applying 4x2 to get total k=8, because all adapters are used and num_classes > 2')
+
+        elif (k_shot == 8 and num_classes < 3 and n_adapters == 16):
+            k_it = 2
+            new_k = 4
+            print('Applying 2x4 to get total k=8, because all adapters are used and num_classes < 3')
+
+
+            # get test dataloaders and sampler
+        full_test_loader, sample_test_loader, sampler = get_test_loaders(task, K_SHOT=new_k,
                                                                          full_dl_batch_size=full_dl_batch_size,
                                                                          num_workers=0)
         # Select the k-shot batch and finetune
@@ -130,7 +90,7 @@ def test_protomaml(model, task, k_shot=4, max_it=20, full_dl_batch_size=8):
                               'attention_mask': x[1][2].to(device)}
             support_targets = x[2].to(device)
 
-            print('iteration:' , i)
+            print('Iteration:' , i)
 
             # Finetune new model on support set
             local_model, output_weight, output_bias, classes = model.adapt_few_shot(
@@ -140,16 +100,15 @@ def test_protomaml(model, task, k_shot=4, max_it=20, full_dl_batch_size=8):
             model.model = local_model
             indices.append(support_indices)
 
-            if i == 4:
-                print('evaluating on local model...')
+            if i == k_it:
+                print('Evaluating on local model...')
                 i = 0
                 support_indices = [item for sublist in indices for item in sublist]
                 indices = []
-                model = model_clean
-                print('support indices:', support_indices)
+                model = deepcopy(model_clean)
+                print('Support indices:', support_indices)
 
                 ev_i += 1
-
                 # get accuracy of finetuned model on full dataset
                 with torch.no_grad():
                     local_model.eval()
@@ -171,8 +130,15 @@ def test_protomaml(model, task, k_shot=4, max_it=20, full_dl_batch_size=8):
                     # exclude support set elements
                     for s_idx in support_indices:
                         batch_acc[s_idx] = 0
+
+                    n_duplicates = len(support_indices) - len(set(support_indices))
+
+                    # only with cb we should have duplicates
+                    if task != 'cb':
+                        assert n_duplicates == 0
+
                     batch_acc = batch_acc.sum().item() / (
-                            batch_acc.shape[0] - len(support_indices)
+                            batch_acc.shape[0] - (len(support_indices) - n_duplicates)
                     )
                     accuracies.append(batch_acc)
 
@@ -181,6 +147,7 @@ def test_protomaml(model, task, k_shot=4, max_it=20, full_dl_batch_size=8):
                     return mean(accuracies), stdev(accuracies)
 
     else:
+
         print('Applying normal meta-testing')
 
         # get test dataloaders and sampler
