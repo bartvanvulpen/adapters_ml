910d303787fd98c3d5f69200570d8abbea80fe2d
Rene Emmaneel <reneemmaneel00@gmail.com>
2022-05-30 14:10:17 +0200

    Added argparse arguments, changed to validation split

--- baseline_test.py
+++ baseline_test.py
@@ -3,12 +3,14 @@ from baseline_dataset import collate_fn
 from torch.utils.data import DataLoader
 from dataset_loader import load_dataset_from_file
 from baseline_module import BaselineModel
+from data_construction import get_validation_split_name
 import pytorch_lightning as pl
 import torch
 from experiments import experiments
 import pickle
-
-torch.manual_seed(0)
+import argparse
+import numpy as np
+import json
 
 def dict_to_device(dic, device):
     """
@@ -20,80 +22,110 @@ def dict_to_device(dic, device):
 
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
-# we perform 10 update steps on the few-shot train data. This number was found to work
-# well for all k values (tested in a hyperparam search)
-n_updates = 10
+if __name__ == "__main__":
+    # using k=2, batch_size=1 and 4 tasks, the model sees 2*2*4*1=16 examples
+    # per update step (assuming 2 classes per task)
+    batch_size=1
 
-# this list will contain all results
-results = []
+    parser = argparse.ArgumentParser()
 
-for experiment in experiments:
-
-    exp_num = experiment['exp_number']
-    test_tasks = experiment['test_tasks']
-
-    for test_task in test_tasks:
-        for k in [2,4,8]:
-
-            print('------------------')
-            print('Start new evaluation with values:')
-            print('Experiment number =', exp_num)
-            print('Test task =', test_task)
-            print('k =', k)
-            
-
-            dataset, id2label = load_dataset_from_file(test_task)
-            n_classes = len(id2label)
-
-            model = BaselineModel.load_from_checkpoint('models/Experiment=' + str(exp_num)+'.ckpt')
-
-            dl = DataLoader(dataset['test'], 
-                    batch_size=k * n_classes, 
-                    shuffle=True,
-                    collate_fn=collate_fn,
-                    drop_last=True
-            )
-
-            data_iterator = iter(dl)
-
-            # get the first batch to train the model
-            train_batch = next(data_iterator)
-            dict_to_device(train_batch, device)
-            
-            model.prepare_for_test(test_task)
-            model.to(device)
-
-            optimizer = model.configure_optimizers()
-
-            model.train()
-            for _ in range(n_updates):
-                optimizer.zero_grad()
-                loss = model.train_step_single_task(train_batch)
-                loss.backward()
-                optimizer.step()
+    parser.add_argument('--tasks', nargs="+", default=["1", "3", "4", "5", "6", "7", "8", "9"],
+                        help='Experiments numbers to train', required=False)
+    parser.add_argument('--n_updates', default=10, type=int,
+                        help='Number of update steps', required=False)
+    parser.add_argument('--outfile', default='baseline_test_results.json', type=str,
+                        help='File to save the results to', required=False)
 
 
-            model.eval()
-            accuracies = []
-
-            # loop over the remaining batches to compute accuracy
-            for batch in data_iterator:
-                dict_to_device(batch, device)
-                accuracies.append(model.compute_accuracy(batch))
 
 
-            accuracy = sum(accuracies) / len(accuracies)
-            print('Accuracy =', accuracy)
 
-            results.append({
-                'exp_number': exp_num,
-                'test_task': test_task,
-                'k': k,
-                'accuracy': accuracy
-            })
+    args = parser.parse_args()
 
-            # save the results every iteration so we can recover
-            # results if an error occurs
-            with open('test_results.pickle', 'wb') as f:
-                pickle.dump(results, f)
+    # this list will contain all results
+    results = []
 
+    for experiment_str in args.tasks:
+        experiment = None
+        for exp in experiments:
+            if exp['exp_number'] == int(experiment_str):
+                experiment = exp
+                break
+        if not experiment:
+            print('Exp number not found!')
+            break
+
+        exp_num = experiment['exp_number']
+        test_tasks = experiment['test_tasks']
+
+        for test_task in test_tasks:
+            for k in [2,4,8]:
+                print('------------------')
+                print('Start new evaluation with values:')
+                print('Experiment number =', exp_num)
+                print('Test task =', test_task)
+                print('k =', k)
+                all_accuracies = []
+                for i in range(20):
+                    torch.manual_seed(i)
+
+                    dataset, id2label = load_dataset_from_file(test_task)
+                    n_classes = len(id2label)
+
+                    model = BaselineModel.load_from_checkpoint('models/Experiment=' + str(exp_num)+'.ckpt')
+
+                    key = get_validation_split_name(test_task)
+                    dl = DataLoader(dataset[key], #Take the validation split
+                            batch_size=k * n_classes,
+                            shuffle=True,
+                            collate_fn=collate_fn,
+                            drop_last=True
+                    )
+
+                    data_iterator = iter(dl)
+
+                    # get the first batch to train the model
+                    train_batch = next(data_iterator)
+                    dict_to_device(train_batch, device)
+
+                    model.prepare_for_test(test_task)
+                    model.to(device)
+
+                    optimizer = model.configure_optimizers()
+
+                    model.train()
+                    for _ in range(args.n_updates):
+                        optimizer.zero_grad()
+                        loss = model.train_step_single_task(train_batch)
+                        loss.backward()
+                        optimizer.step()
+
+
+                    model.eval()
+                    accuracies = []
+
+                    # loop over the remaining batches to compute accuracy
+                    for batch in data_iterator:
+                        dict_to_device(batch, device)
+                        accuracies.append(model.compute_accuracy(batch))
+
+                    accuracy = sum(accuracies) / len(accuracies)
+                    all_accuracies.append(accuracy)
+
+
+                    print(f'iter {i} | accuracy: {accuracy}')
+
+                print(f'avg: {np.average(all_accuracies)}')
+                print(f'avg: {np.std(all_accuracies)}')
+                results.append({
+                    'exp_number': exp_num,
+                    'test_task': test_task,
+                    'k': k,
+                    'accuracy': np.average(all_accuracies),
+                    'std': np.std(all_accuracies)
+                })
+
+                # save the results every iteration so we can recover
+                # results if an error occurs
+                with open(args.outfile, 'w') as f:
+                    json.dump(results, f)
--- baseline_train.py
+++ baseline_train.py
@@ -1,50 +1,67 @@
+import os
+import argparse
 from gc import callbacks
 from torch.utils.data import DataLoader
 from baseline_module import BaselineModel
 from baseline_dataset import MultiTaskDataset
 from pytorch_lightning.callbacks import ModelCheckpoint
 import torch
-from experiments import experiments 
+from experiments import experiments
 
 # import required to enable unpickling Argument dataset
 from dataset_loader import ArgumentDatasetSplit
 
 import pytorch_lightning as pl
 
-k = 4
-# using k=2, batch_size=1 and 4 tasks, the model sees 2*2*4*1=16 examples
-# per update step (assuming 2 classes per task)
-batch_size=1
+if __name__ == "__main__":
+    k = 4
+    # using k=2, batch_size=1 and 4 tasks, the model sees 2*2*4*1=16 examples
+    # per update step (assuming 2 classes per task)
+    batch_size=1
 
-for experiment in experiments:
-    adapter_tasks = experiment['adapter_tasks']
-    train_tasks = experiment['train_tasks']
-    exp_num = experiment['exp_number']
+    parser = argparse.ArgumentParser()
 
-    print('------------------------------')
-    print('START EXPERIMENT', exp_num)
-    print('adapters', adapter_tasks)
-    print('train_tasks', train_tasks)
-    print('k =', k)
-    print('batch_size =', batch_size)
+    parser.add_argument('--tasks', nargs="+", default=["1", "3", "4", "5", "6", "7", "8", "9"],
+                        help='Experiments numbers to train', required=False)
+    parser.add_argument('--k', default=4, type=int,
+                        help='Number of datapoints per label during testing', required=False)
+    parser.add_argument('--batch_size', default=1, type=int,
+                        help='Batch size', required=False)
 
-    dataloader = DataLoader(MultiTaskDataset(train_tasks, k=k), batch_size=batch_size)
 
-    model = BaselineModel(adapter_tasks, train_tasks, k=k, exp_num=exp_num)
+    args = parser.parse_args()
 
-    # save the model with the lowest training loss
-    checkpoint = ModelCheckpoint(
-        monitor='train_loss', 
-        every_n_train_steps=5,
-        filename='Experiment=' + str(exp_num)
-    )
+    for experiment_str in args.tasks:
+        experiment = int(experiment_str)
 
-    trainer = pl.Trainer(
-        gpus=1 if torch.cuda.is_available() else 0, 
-        max_epochs=1, 
-        callbacks=[checkpoint],
-        progress_bar_refresh_rate=100,
-        log_every_n_steps=1
-    )
+        adapter_tasks = experiment['adapter_tasks']
+        train_tasks = experiment['train_tasks']
+        exp_num = experiment['exp_number']
 
-    trainer.fit(model=model, train_dataloaders=dataloader)
+        print('------------------------------')
+        print('START EXPERIMENT', exp_num)
+        print('adapters', adapter_tasks)
+        print('train_tasks', train_tasks)
+        print('k =', args.k)
+        print('batch_size =', args.batch_size)
+
+        dataloader = DataLoader(MultiTaskDataset(train_tasks, k=args.k), batch_size=args.batch_size)
+
+        model = BaselineModel(adapter_tasks, train_tasks, k=args.k, exp_num=exp_num)
+
+        # save the model with the lowest training loss
+        checkpoint = ModelCheckpoint(
+            monitor='train_loss',
+            every_n_train_steps=5,
+            filename='Experiment=' + str(exp_num)
+        )
+
+        trainer = pl.Trainer(
+            gpus=1 if torch.cuda.is_available() else 0,
+            max_epochs=1,
+            callbacks=[checkpoint],
+            progress_bar_refresh_rate=100,
+            log_every_n_steps=1
+        )
+
+        trainer.fit(model=model, train_dataloaders=dataloader)
